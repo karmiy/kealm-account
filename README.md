eab760a64b4619b6e44006ff975cf276cbd4bf4a
karmiy <532981481@qq.com>
2022-03-15 22:16:49 +0800

    chore: 项目初建中（vue-cli 版）

---
+++ README.md
@@ -0,0 +1,21 @@
+# 记账小程序
+
+## 前端项目
+
+### 项目创建
+
+使用 vue-cli 创建，非 HBuilderX，考虑：
+
+- typescript 项目创建
+- eslint prettier 在 VSCode 更好的支持
+
+### 项目运行
+
+HBuilderX 打开项目，运行小程序
+
+
+## 常见问题
+
+### 代码更新小程序出错
+
+需要在 HBuilderX 打开 manifest.json => 微信小程序配置，填写 AppID
---
+++ account-fe/src/pages/home/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <view class="home">
+        1111
+        <text class="title">{{ title }}</text>
+        <button @click="handleOpenCalendar">打开日历</button>
+        <uni-calendar
+            ref="calendarRef"
+            :insert="false"
+            :lunar="true"
+            @confirm="handleCalendarConfirm"
+        />
+    </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const title = ref('Hello11');
+const calendarRef = ref<{ open(): void } | null>(null);
+
+const handleCalendarConfirm = (e: any) => {
+    console.log('e', e);
+};
+
+const handleOpenCalendar = () => {
+    calendarRef.value?.open();
+};
+</script>
+
+<style>
+.content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+</style>
